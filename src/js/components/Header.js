88d256b398c74b4b014d2ec8b6fd3030889cc0a1
YangKe <saint_yk@163.com>
2017-05-03 09:30:02 +0800

    add go backward navbar btn

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,15 +1,26 @@
 import React,{Component} from 'react';
 import { NavBar } from 'amazeui-touch';
 
+const itemLeft = {
+	href: 'javascript:window.history.go(-1);',
+	title: '返回',
+};
 
-let dataAll = {
-  title: ''
+const dataAll = {
+  title: '',
+  leftNav: []
 };
 
 class Header extends Component{
 	render(){
     dataAll.title = this.props.title;
-		return (
+    if(this.props.back){
+    	 dataAll.leftNav = [{...itemLeft}]
+    }
+    else{
+    	dataAll.leftNav = [];
+    }
+	return (
   		<NavBar {...dataAll} amStyle="success" />
 		);
 	}
--- src/js/components/Service.js
+++ src/js/components/Service.js
@@ -1,16 +1,26 @@
 import React,{Component} from 'react';
 import AppStore from '../stores/AppStore.js';
+import Header from './Header.js';
+import {
+	Container 
+} from 'amazeui-touch';
+
+function getServiceDetail(props){
+	let id = props.match.params.id;
+	console.log('id',id);
+	return {};
+}
 
 class Service extends Component{
 	constructor(props){
 		super(props);
-		this.state = {
-			
-		}
+		this.state = getServiceDetail(props);
 	}
 	render(){
 		return (<div>
-			Service
+			<Container>
+				<Header title="物业服务" back="true" />
+			</Container>
 		</div>);
 	}
 }
