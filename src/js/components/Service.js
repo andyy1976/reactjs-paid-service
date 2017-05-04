66e5cf139900eb7e4c38ce8b7c71fa6b7d0054f7
YangKe <saint_yk@163.com>
2017-05-04 10:42:36 +0800

    servie component add form

--- src/js/components/Service.js
+++ src/js/components/Service.js
@@ -2,6 +2,11 @@ import React,{Component} from 'react';
 import AppStore from '../stores/AppStore.js';
 import Header from './Header.js';
 import {
+	Group,
+	Card,
+	List,
+  	Field,
+  	Button,
 	Container 
 } from 'amazeui-touch';
 import AppAPI from '../utils/AppAPI.js';
@@ -30,15 +35,76 @@ class Service extends Component{
 		AppStore.removeChangeListener(this.onChange);
 	}
 	render(){
+		console.log('here',this.state, this.state === null );
+		let content = '';
+		if( this.state !== null){
+			content = this.state.service.name + '<br/>'
+				+ this.state.service.desc + '<br/>'
+				+ '¥' + this.state.service.price;
+		}
 		return (<div>
 			<Container>
 				<Header title="物业服务" back="true" />
+
+				<Card title="服务项目">
+		         	<div dangerouslySetInnerHTML={{__html: content}}>
+		         	</div>
+		        </Card>
+
+
+		        <Card title="联系信息">
+		        	<List>
+			        	 <List.Item
+		                  key={0}
+		                  nested="input"
+		                >
+			        		<Field 
+			        			label="姓名"
+			        			type="text"
+			        			defaultValue="张三"
+			        		/>
+			        	</List.Item>
+			        	<List.Item
+		                  key={1}
+		                  nested="input"
+		                >
+			        		<Field 
+			        			label="电话"
+			        			type="text"
+			        			defaultValue="1388888888"
+			        		/>
+			        	</List.Item>
+			        	<List.Item
+		                  key={2}
+		                  nested="input"
+		                >
+			        		<Field 
+			        			label="需求"
+			        			type="textarea"
+			        		/>
+			        	</List.Item>
+			        	<List.Item
+		                  key={3}
+		                >
+		                	<Field
+					            value="提交"
+					            type="submit"
+					            amStyle="success"
+					            block
+					            onClick={this.handleSubmit}
+					          />
+		                </List.Item>
+		        	</List>
+	        	</Card>
 			</Container>
 		</div>);
 	}
 	_onChange(){
 		let state = getAppState();
 		this.setState(state);
+	}
+	handleSubmit(){
+		console.log('submit');
 	}
 }
 
